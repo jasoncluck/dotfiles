0e05a1d6c83feb20ec1c6f40c20dc4ccca99fb7d
Jason Cluck <jasoncluck@gmail.com>
2024-06-22 19:33:14 -0700

    Break out part of init.lua into modules

---
+++ dotfiles/config/nvim/lua/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+	require 'plugins.conform',
+	require 'plugins.gitsigns',
+	require 'plugins.lspconfig',
+	require 'plugins.smart-splits',
+	require 'plugins.telescope',
+	require 'plugins.which-key'
+}
